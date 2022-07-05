7cc1ce58df02d67ec44328c8ddee8b293073c88b
Scarlet Kim <scarletkim714@gmail.com>
2022-07-05 19:33:27 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 3;
+var height = 2;
+var area = width * height;
+console.log('Value of area:', area);
+console.log('Type of area:', typeof area);
+
+var bill = 20;
+var payment = 30;
+var change = payment - bill;
+console.log('Value of change:', change);
+console.log('Type of change:', typeof change);
+
+var quizzes = 15;
+var midterm = 25;
+var final = 50;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of grade:', grade);
+console.log('Type of grade:', typeof grade);
+
+var firstName = 'Scarlet';
+var lastName = 'Kim';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName:', fullName);
+console.log('Type of fullName:', typeof fullName);
+
+var pH = 7;
+var isAcidic = pH < 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('Type of isAcidic:', typeof isAcidic);
+
+var headCount = 2;
+var isSparta = headCount === 300;
+console.log('Value of isSparta:', isSparta);
+console.log('Type of isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('Value of motto:', motto);
+console.log('Type of motto:', typeof motto);
