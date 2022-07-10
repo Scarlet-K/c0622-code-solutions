da47934a964c1c8478cd528fdae4bb9e4c960832
Scarlet Kim <scarletkim714@gmail.com>
2022-07-10 22:03:21 +0000

    In progress

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,17 @@
 /* exported firstChars */
+
+// create a variable with empty string
+// create a for loop
+// the condition expression of the for loop should be less than the given length
+// create a conditional inside the for loop to check whether the string[i] is valid
+// return the concatenation of the result of the loop and the variable
+
+function firstChars(length, string) {
+  var newString = '';
+  for (var i = 0; i < length; i++) {
+    if (string[i]) {
+      newString += string[i];
+    }
+  }
+  return newString;
+}
--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,1 +1,18 @@
 /* exported lastChars */
+
+// create a variable with empty string
+// create a for loop
+// set the initial expression as the last character of the string = length - 1
+// set the condition expression as greater than or equal to 0
+// set the final expression to decrement the counter variable
+// concatenate string[i] with variable
+// return variable
+
+function lastChars(length, string) {
+  var lastChars = '';
+  // console.log(string[string.length - 1]);
+  // for (var i = ; i >= length; i--) {
+  //   lastChars += string[i];
+  // }
+  return lastChars;
+}
