45d5604f107b33fef0c2242ccac8c373075d283d
Scarlet Kim <scarletkim714@gmail.com>
2022-07-10 05:47:36 +0000

    In progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,21 @@
 /* exported capitalizeWords */
+
+// every first character of a word is uppercase
+// the first word's first character is always at index 0
+// every next word starts after a space
+// every next word's first character is always after space
+
+function capitalizeWords(string) {
+  var newString = string[0].toUpperCase();
+  // newString = 'N'
+  for (var i = 1; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      newString += string[i];
+      // newString = 'Needssolution'
+    } else {
+      newString += string[i + 1].toUpperCase();
+      // newString = 'NeedsSsolution'
+    }
+  }
+  return newString;
+}
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,3 +1,20 @@
 /* exported ransomCase */
 
 // every other character = odd index
+// create an empty string
+// loop over each character
+// turn all even characters lowercased and add it to the empty string
+// turn all odd characters uppercased and add it to the empty string
+// return the new string
+
+function ransomCase(string) {
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i % 2 === 0) {
+      newString += string[i].toLowerCase();
+    } else {
+      newString += string[i].toUpperCase();
+    }
+  }
+  return newString;
+}
