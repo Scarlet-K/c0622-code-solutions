6dfec5fd38b9073f494107e97eff5ff3e899a842
Scarlet Kim <scarletkim714@gmail.com>
2022-07-12 00:08:14 +0000

    In Progress

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -2,17 +2,20 @@
 
 // create a variable with empty string
 // create a for loop
-// set the initial expression as the last character of the string = length - 1
-// set the condition expression as greater than or equal to 0
+// start the loop at the last character
 // set the final expression to decrement the counter variable
+// the length of the string - given length = the index of the last character = when to STOP
 // concatenate string[i] with variable
+// loop over the new string
+// start the loop at the last character
 // return variable
 
 function lastChars(length, string) {
   var lastChars = '';
-  // console.log(string[string.length - 1]);
-  // for (var i = ; i >= length; i--) {
-  //   lastChars += string[i];
-  // }
+  for (var i = string.length - 1; i >= string.length - length; i--) {
+    if (string[i]) {
+      lastChars += string[i];
+    }
+  }
   return lastChars;
 }
