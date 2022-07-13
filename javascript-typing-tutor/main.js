65a9fd37ed046522177be16b9eab66e465779080
Scarlet Kim <scarletkim714@gmail.com>
2022-07-13 20:15:25 +0000

    In progress: how to stop the keydown event after last character is checked correct

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,21 +1,18 @@
-// query for all elements that are span
-// store the nodelist in a variable
-// when used in keyboard event:
-// event.key = returns the value of the key pressed
-//
-
 var $characters = document.querySelectorAll('span');
 var counter = 0;
+
 window.addEventListener('keydown', handleKeydown);
 
 function handleKeydown(event) {
-  if (($characters[counter].textContent === event.key) && (counter !== $characters.length - 1)) {
+  // if ((counter >= $characters.length - 1) && ($characters[counter].textContent === event.key)) {
+  //   $characters[counter].className = 'correct';
+  //   return;
+  // }
+  if (($characters[counter].textContent === event.key) && (counter < $characters.length)) {
     $characters[counter].className = 'correct';
     $characters[counter + 1].className = 'border-bottom';
     counter++;
-  } else if (($characters[counter].textContent !== event.key) && (counter !== $characters.length - 1)) {
+  } else if (($characters[counter].textContent !== event.key) && (counter < $characters.length)) {
     $characters[counter].className = 'incorrect';
-  } else if (counter === $characters.length - 1) {
-    event.preventDefault();
   }
 }
