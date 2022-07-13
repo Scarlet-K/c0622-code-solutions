b95bd998fe50afabd8de3ab1729e5be2dfd47858
Scarlet Kim <scarletkim714@gmail.com>
2022-07-13 21:32:12 +0000

    In progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -8,13 +8,30 @@
 // attach the uppercased first character of the new word that comes after the space
 //
 
+// the outer loop goes through the string and concatenates to the firstChar until it hits a space
+// if the current characters is a space, slice the string at the space
+// else
+// the inner loop concatenates the character after the space to the firstChar
+// stop at next char
+
 function capitalizeWords(string) {
   var firstChar1 = string[0].toUpperCase();
   for (var i = 1; i < string.length; i++) {
     firstChar1 += string[i].toLowerCase();
     if (string[i] === ' ') {
-      firstChar1 += string.slice(i + 1, i + 2).toUpperCase();
+      firstChar1 += string.slice(i);
     }
   }
   return firstChar1;
 }
+
+// function capitalizeWords(string) {
+//   var firstChar1 = string[0].toUpperCase();
+//   for (var i = 1; i < string.length; i++) {
+//     firstChar1 += string[i].toLowerCase();
+//     if (string[i] === ' ') {
+//       firstChar1 += string.slice(i + 1, i + 2).toUpperCase();
+//     }
+//   }
+//   return firstChar1;
+// }
