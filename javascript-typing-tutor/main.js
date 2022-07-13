8114a6217ea3e971415491ac238e29da51d84478
Scarlet Kim <scarletkim714@gmail.com>
2022-07-13 02:08:33 +0000

    In progress

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,2 +1,13 @@
 // query for all elements that are span
+// store the nodelist in a variable
+// when used in keyboard event:
+// event.key = returns the value of the key pressed
 //
+
+var $container = document.querySelectorAll('.container');
+
+$container.addEventListener('keydown', handleKeydown);
+
+function handleKeydown() {
+  return 0;
+}
