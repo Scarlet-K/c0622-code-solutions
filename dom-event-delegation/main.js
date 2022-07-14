8877a66f39a5905d9188ee8807425df1bc8eda66
Scarlet Kim <scarletkim714@gmail.com>
2022-07-14 19:00:43 +0000

    Completed dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,14 @@
+var $taskList = document.querySelector('.task-list');
+
+$taskList.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  console.log('event.target:', event.target);
+  console.log('event.target.tagName:', event.target.tagName);
+
+  if (event.target.matches('button')) {
+    var $taskItem = event.target.closest('.task-list-item');
+    console.log('  closest . task-list-item:', $taskItem);
+    $taskItem.remove();
+  }
+}
