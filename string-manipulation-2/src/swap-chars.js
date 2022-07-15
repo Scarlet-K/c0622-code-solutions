d44a1d20537ead9b9d9838f7832a50b6bdde70f8
Scarlet Kim <scarletkim714@gmail.com>
2022-07-15 16:59:56 +0000

    In progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -5,17 +5,26 @@
 // loop over the string
 // if the index matches the firstIndex
 
-function swapChars(firstIndex, secondIndex, string) {
-  var first = [string[firstIndex]];
-  var second = [string[secondIndex]];
-  var stringArray = [];
-  for (var i = 0; i < string.length; i++) {
-    stringArray.push(string[i]);
-    if (string[i] === string[firstIndex]) {
-      stringArray.push(second);
-    } else if (string[i] === string[secondIndex]) {
-      stringArray.push(first);
-    }
-  }
-  return stringArray.join('');
-}
+// replace the firstIndex with secondIndex
+// var first = 'a'
+// var second = b
+// first = second
+// firstletter = b
+// second = b
+// second = first
+// string is immutable!
+
+// function swapChars(firstIndex, secondIndex, string) {
+//   var first = [string[firstIndex]];
+//   var second = [string[secondIndex]];
+//   var stringArray = [];
+//   for (var i = 0; i < string.length; i++) {
+//     stringArray.push(string[i]);
+//     if (string[i] === string[firstIndex]) {
+//       stringArray.push(second);
+//     } else if (string[i] === string[secondIndex]) {
+//       stringArray.push(first);
+//     }
+//   }
+//   return stringArray.join('');
+// }
