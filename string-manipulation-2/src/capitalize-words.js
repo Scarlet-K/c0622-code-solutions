d82622ccc64503ad6352d0e620264d39e2282aaf
Scarlet Kim <scarletkim714@gmail.com>
2022-07-15 01:51:45 +0000

    In progress

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,42 +1,28 @@
 /* exported capitalizeWords */
 
-// slice returns what is cut out from the string
-// slice at every space
-// concatenate the return with the first index uppercased
-
-// every first character of a word is uppercase
-// the first word's first character is always at index 0
-// every next word starts after a space
-// every next word's first character is always after space
-// cut the string after a space = slice
-// attach the uppercased first character of the new word that comes after the space
+// upperCase and lowerCase returns what is changed
+// slice returns what is sliced
 //
 
-// the outer loop goes through the string and concatenates to the firstChar until it hits a space
-// if the current characters is a space, slice the string at the space
-// else
-// the inner loop concatenates the character after the space to the firstChar
-// stop at next char
-
-function capitalizeWords(string) {
-  var firstChar1 = string[0].toUpperCase();
-  for (var i = 1; i < string.length; i++) {
-    if (string[i] !== ' ') {
-      firstChar1 += string[i].toLowerCase();
-    } else if (string[i] === ' ') {
-      firstChar1 += string.slice(i, i + 2).toUpperCase();
-    }
-  }
-  return firstChar1;
-}
+// add each character lowercased as long as it isn't after a space
+// check if a characters is a space
+// if the characters is a space
 
 // function capitalizeWords(string) {
 //   var firstChar1 = string[0].toUpperCase();
+//   var sliced = '';
 //   for (var i = 1; i < string.length; i++) {
-//     firstChar1 += string[i].toLowerCase();
 //     if (string[i] === ' ') {
-//       firstChar1 += string.slice(i + 1, i + 2).toUpperCase();
+//       for (var k = 0; k < firstChar1.length; k++) {
+//         if (string[k] === ' ') {
+//           sliced += string.slice(k, k + 2).toUpperCase();
+//           break;
+//         }
+//       }
+//     } else {
+//       firstChar1 += string[i].toLowerCase();
 //     }
+//     firstChar1 += sliced;
 //   }
 //   return firstChar1;
 // }
