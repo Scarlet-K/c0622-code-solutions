c337f9aceee5b778f7abefd1ce85ee5a54ce4738
Scarlet Kim <scarletkim714@gmail.com>
2022-07-25 22:51:40 +0000

    Completed array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,20 +1,16 @@
 /* exported chunk */
 
-// create an empty array called outer array
-// create a empty array called inner array
-// the first loop starts at the first character
-// at each iteration push the current character into the outer array
-// the first loop stops at the last character within the size
-// the second loop starts where the outer loop stopped
-// at each iteration plus the current character into the inner array
-// the second loop stops at the last character of the entire array
-// push the inner array into the outer array
+// create an empty array
+// loop over the array
+// increment the i by the size
+// slice the array at the current index up to the index + size
+// push the returned array into the final array
+// return the final array
 
-// function chunk(array, size) {
-//   debugger;
-//   var newArray = [];
-//   for (var i = 0; i < array.length; i++) {
-//     newArray.push(array.slice(array[i], size));
-//   }
-//   return newArray;
-// }
+function chunk(array, size) {
+  var finalArray = [];
+  for (var i = 0; i < array.length; i += size) {
+    finalArray.push(array.slice(i, size + i));
+  }
+  return finalArray;
+}
