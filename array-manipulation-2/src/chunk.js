4d2831ca1b70d28e26a38e4650c3ce0430b7d9df
Scarlet Kim <scarletkim714@gmail.com>
2022-07-25 06:35:09 +0000

    In progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -6,6 +6,15 @@
 // at each iteration push the current character into the outer array
 // the first loop stops at the last character within the size
 // the second loop starts where the outer loop stopped
-// at each iteration pus the current character into the inner array
+// at each iteration plus the current character into the inner array
 // the second loop stops at the last character of the entire array
 // push the inner array into the outer array
+
+// function chunk(array, size) {
+//   debugger;
+//   var newArray = [];
+//   for (var i = 0; i < array.length; i++) {
+//     newArray.push(array.slice(array[i], size));
+//   }
+//   return newArray;
+// }
