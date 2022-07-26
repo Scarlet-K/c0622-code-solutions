2d41a64f1d6723c56560b5c56bc77cc5f20a2cce
Scarlet Kim <scarletkim714@gmail.com>
2022-07-26 22:55:27 +0000

    fixed drop-right by stopping the loop at the desired length.

--- array-manipulation-2/src/drop-right.js
+++ array-manipulation-2/src/drop-right.js
@@ -2,26 +2,14 @@
 
 // create a new array
 // loop over the array
-// check BEFORE the loop whether the given count is greater than the length of the array
-// if true, return a shallow copy of the entire array
-// if false, start loop
-// start at the end of the array
-// determine the range by subtracting the highest value (starting point) with the lowest value (stopping point)
-// if the index falls within the range, continue the loop without any action
-// else, push every new item before the previous item
-// return the new array
+// stop when the current index is greater than the length - count
+// push the current index at every iteration
+// return newArray
 
 function dropRight(array, count) {
   var newArray = [];
-  if (count > array.length - 1) {
-    return array.slice(0, count);
-  }
-  for (var i = array.length - 1; i >= 0; i--) {
-    if (i >= array.length - count) {
-      continue;
-    } else {
-      newArray.unshift(array[i]);
-    }
+  for (var i = 0; i < array.length - count; i++) {
+    newArray.push(array[i]);
   }
   return newArray;
 }
