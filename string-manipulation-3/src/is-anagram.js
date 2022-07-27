44d8b3fa2e3f97cbd367e5f1c35a51dab5b29614
Scarlet Kim <scarletkim714@gmail.com>
2022-07-27 01:26:33 +0000

    In progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,26 +1,23 @@
 /* exported isAnagram */
 
-// create a new string that omits spaces
-// loop over the firstString
-// check if the current index of firstString appears anywhere in the second string
-//
-// function isAnagram(firstString, secondString) {
-//   debugger;
-//   var newfirstString = firstString.replaceAll(' ', '');
-//   var newsecondString = secondString.replaceAll(' ', '');
-//   for (var i = 0; i < newfirstString.length; i++) {
-//     for (var k = 0; k < newsecondString.length; k++) {
-//       if (newsecondString[i] !== newfirstString[k]) {
-//         continue;
-//       } else {
-//         newfirstString.replace(newfirstString[k], '');
-//         break;
-//       }
-//     }
-//   }
-//   if (newfirstString === '') {
-//     return true + newfirstString;
-//   } else {
-//     return false + newfirstString;
-//   }
-// }
+// compare the letters inside secondString with letters inside firstString
+// if the current letter is inside the firstString
+// get rid of the current letter inside the secondString
+// get rid of the matching letter inside the firstString
+// if the length of both strings are equal to 0
+// return true
+// else return false
+
+function isAnagram(firstString, secondString) {
+  var newFirst = firstString.replaceAll(' ', '');
+  var newSecond = secondString.replaceAll(' ', '');
+  var slicedFirst = '';
+  for (var i = 0; i < newSecond.length; i++) {
+    for (var k = 0; k < newFirst.length; k++) {
+      if (newSecond[i] === newFirst[k]) {
+        slicedFirst += newFirst.slice(k, k + 1);
+      }
+    }
+  }
+  return slicedFirst;
+}
