5a4fe6c7b68c8583def9521e474cd3e80fbf78e1
Scarlet Kim <scarletkim714@gmail.com>
2022-07-28 00:59:08 +0000

    In progress: changing circles

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,39 +1,67 @@
-// push all images into an array OR give each image element an ID#
-// add an eventlistener on the chevron arrows
-// add an eventlistener on the dots
-// keep track of the current image OR the current dot
-// the current image is the same as current dot.
-
 var images = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
-var $imgList = document.querySelectorAll('.view');
-// console.log($imgList);
-for (var i = 0; i < $imgList.length; i++) {
-  // console.log($imgList[i].getAttribute('data-view'));
-}
-
+var intervalID = setInterval(getNextIndex, 3000);
+var index = 0;
 var $leftArrow = document.querySelector('.fa-chevron-left');
 var $rightArrow = document.querySelector('.fa-chevron-right');
 var $img = document.querySelector('img');
+var $circles = document.querySelectorAll('.fa-circle');
+var $circleContainer = document.querySelector('.circle-container');
 
 $leftArrow.addEventListener('click', showPreviousImage);
 $rightArrow.addEventListener('click', showNextImage);
+$circleContainer.addEventListener('click', handleCircles);
 
 function showPreviousImage(event) {
-  for (var i = 0; i < $imgList.length; i++) {
-    // var viewID = $imgList[i].getAttribute('data-view');
-    // console.log(viewID);
-  }
+  clearInterval(intervalID);
+  getPreviousIndex();
 }
 
 function showNextImage(event) {
-  var counter = 0;
-  $img.setAttribute('src', images[counter + 1]);
+  clearInterval(intervalID);
+  getNextIndex();
 }
 
-// function getPreviousIndex () {
+function handleCircles(event) {
+  clearInterval(intervalID);
+  if (event.target.tagName === 'I') {
+    var dataIndex = event.target.getAttribute('data-index');
+    for (var i = 0; i < images.length; i++) {
+      if (dataIndex === JSON.stringify(i)) {
+        $img.setAttribute('src', images[i]);
+        $circles[i].classList.add('fa-solid');
+        $circles[i].classList.remove('fa-regular');
+      } else {
+        $circles[i].classList.add('fa-regular');
+        $circles[i].classList.remove('fa-solid');
+      }
+    }
+  }
+}
 
-// }
+function getPreviousIndex() {
+  if (index === 0) {
+    index = images.length - 1;
+    $img.setAttribute('src', images[index]);
+  } else {
+    index--;
+    $img.setAttribute('src', images[index]);
+  }
+}
 
-// function getNextIndex
+function getNextIndex() {
+  if (index === images.length - 1) {
+    index = 0;
+    $img.setAttribute('src', images[0]);
+    $circles[0].classList.add('fa-solid');
+    $circles[0].classList.remove('fa-regular');
+  } else {
+    index++;
+    $img.setAttribute('src', images[index]);
+    $circles[index].classList.add('fa-solid');
+    $circles[index].classList.remove('fa-regular');
+    $circles[index - 1].add('fa-regular');
+    $circles[index - 1].remove('fa-solid');
+  }
+}
 
 // function setIndex
