40f07c960d9bd92a096aeb01e42aef4b9a39a5b4
Scarlet Kim <scarletkim714@gmail.com>
2022-08-17 18:40:01 +0000

    create const variable for numbers outside of conditional statements, adjust these statements to log error message if invalid operator.

--- node-require/calculate.js
+++ node-require/calculate.js
@@ -4,27 +4,17 @@ const divide = require('./divide');
 const multiply = require('./multiply');
 
 const operator = process.argv[3];
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[4]);
 
 if (operator === 'plus') {
-  const num1 = parseInt(process.argv[2]);
-  const num2 = parseInt(process.argv[4]);
   console.log('result:', add(num1, num2));
-}
-
-if (operator === 'minus') {
-  const num1 = parseInt(process.argv[2]);
-  const num2 = parseInt(process.argv[4]);
+} else if (operator === 'minus') {
   console.log('result:', subtract(num1, num2));
-}
-
-if (operator === 'over') {
-  const num1 = parseInt(process.argv[2]);
-  const num2 = parseInt(process.argv[4]);
+} else if (operator === 'over') {
   console.log('result:', divide(num1, num2));
-}
-
-if (operator === 'times') {
-  const num1 = parseInt(process.argv[2]);
-  const num2 = parseInt(process.argv[4]);
+} else if (operator === 'times') {
   console.log('result:', multiply(num1, num2));
+} else {
+  console.log('invalid operation');
 }
