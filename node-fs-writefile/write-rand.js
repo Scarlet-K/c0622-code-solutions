fe18df9bc303eedbc8c9b015ebfc7e882a4a9a41
Scarlet Kim <scarletkim714@gmail.com>
2022-08-18 17:22:12 +0000

    code comment with alternative way.

--- node-fs-writefile/write-rand.js
+++ node-fs-writefile/write-rand.js
@@ -1,6 +1,15 @@
 const fs = require('fs');
 const data = Math.random().toString() + '\n';
 
+// const randomNumber = Math.random();
+
 fs.writeFile('random.txt', data, err => {
   if (err) throw err;
 });
+
+// fs.writeFile('random.txt', randomNumber + '\n', 'utf8', err => {
+//   if (err) {
+//     console.error(err);
+//     process.exit(1);
+//   }
+// });
