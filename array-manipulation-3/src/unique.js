fa5b9226bd1b0f8f536d33bd11ca2e4894bc0106
Scarlet Kim <scarletkim714@gmail.com>
2022-08-19 17:02:57 +0000

    still in progress.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,10 +1,13 @@
 /* exported unique */
 
-// Create an empty array
-// Check the first index’s value with all the other index’s value
-// If the first index’s value match with any other index’s value stop checking
-// Else, keep checking until the end of the array and push the current value into the empty array.
-// Return the final array
+// 1. Create an empty array
+// 2. Grab the first index
+// 3. Go over the array starting from the next index over all the way to the last index
+// 4. IF the first index’s value MATCH with any other index’s value stop checking
+// 5. Grab the second index
+// 6. Repeat the #3
+// 7. ELSE, keep checking until the end of the array and push the current value into the empty array
+// 8. Return the final array
 
 function unique(array) {
   // debugger;
