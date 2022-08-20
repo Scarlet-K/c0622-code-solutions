89fce7de7ca7c692c3a72d8a3c7e6f165f6df039
Scarlet Kim <scarletkim714@gmail.com>
2022-08-20 10:03:37 +0000

    In progress

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,25 +1,22 @@
 /* exported unique */
 
-// 1. Create an empty array
-// 2. Grab the first index
-// 3. Go over the array starting from the next index over all the way to the last index
-// 4. IF the first index’s value MATCH with any other index’s value stop checking
-// 5. Grab the second index
-// 6. Repeat the #3
-// 7. ELSE, keep checking until the end of the array and push the current value into the empty array
-// 8. Return the final array
+// create a copy of the given array
+// grab the first index of the array
+// check the first index with the next index in order
+// if it is a match, splice the matching index and remove it from the array
+// do this until the end of the array
+// grab the second index of the array
+// repeat the process
+// return the altered copied array
 
 function unique(array) {
-  // debugger;
-  var finalArray = [];
-  for (var i = 0; i < array.length; i++) {
-    for (var k = i + 1; k < array.length; k++) {
-      if (array[i] === array[k]) {
-        break;
-      } else {
-        finalArray.push(array[i]);
+  var newArray = array;
+  for (var i = 0; i < newArray.length; i++) {
+    for (var k = i + 1; k < newArray.length - 1; k++) {
+      if (newArray[i] === newArray[k]) {
+        newArray.splice(k, 1);
       }
     }
   }
-  return finalArray;
+  return newArray;
 }
