0a15e00179593b8b573b1d17e5cc47c6c179c875
Scarlet Kim <scarletkim714@gmail.com>
2022-08-20 11:35:36 +0000

    Completed: unique

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,12 @@
 /* exported difference */
+// function difference(first, second) {
+//   var differenceArray = [];
+//   for (var i = 0; i < second.length; i++) {
+//     for (var k = 0; k < first.length; k++) {
+//       if (second[i] !== first[k]) {
+//         differenceArray.push(first[i]);
+//       }
+//     }
+//   }
+//   return differenceArray;
+// }
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,21 +1,15 @@
 /* exported unique */
 
-// create a copy of the given array
-// grab the first index of the array
-// check the first index with the next index in order
-// if it is a match, splice the matching index and remove it from the array
-// do this until the end of the array
-// grab the second index of the array
-// repeat the process
-// return the altered copied array
+// create an empty new array
+// check if the newArray includes the items in array
+// only add the item if it is NOT included
+// return the new array
 
 function unique(array) {
-  var newArray = array;
-  for (var i = 0; i < newArray.length; i++) {
-    for (var k = i + 1; k < newArray.length - 1; k++) {
-      if (newArray[i] === newArray[k]) {
-        newArray.splice(k, 1);
-      }
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (!newArray.includes(array[i])) {
+      newArray.push(array[i]);
     }
   }
   return newArray;
