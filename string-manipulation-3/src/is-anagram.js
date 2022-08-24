7cfa8712a045c27f3e835059f73b56590655ee95
Scarlet Kim <scarletkim714@gmail.com>
2022-08-24 09:22:31 +0000

    In progress

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,23 +1,46 @@
 /* exported isAnagram */
 
-// compare the letters inside secondString with letters inside firstString
-// if the current letter is inside the firstString
-// get rid of the current letter inside the secondString
-// get rid of the matching letter inside the firstString
-// if the length of both strings are equal to 0
-// return true
-// else return false
+// Create an empty object for firstString
+// Create an empty object for secondString
+// Go over the characters in the firstString
+// At each character check if the object already has the property
+//   If yes, increment its value by 1
+//   Else, assign the current character as the property of the empty object with the value of 1
+// Do the same for the secondString
+// After going over all the characters in both first and second string, we should have two objects
+// These two objects should have all characters as their properties and values that match how many of the characters appear in the string
+// Go over all the properties of the two objects(which are characters in the strings)
+// Compare the two objects by checking the properties and values
+// If all the values AND values match, return true
+// Else return false
 
 function isAnagram(firstString, secondString) {
+  // debugger;
   var newFirst = firstString.replaceAll(' ', '');
   var newSecond = secondString.replaceAll(' ', '');
-  var slicedFirst = '';
-  for (var i = 0; i < newSecond.length; i++) {
-    for (var k = 0; k < newFirst.length; k++) {
-      if (newSecond[i] === newFirst[k]) {
-        slicedFirst += newFirst.slice(k, k + 1);
-      }
+  var first = {};
+  var second = {};
+  var result = false;
+  if (newFirst.length !== newSecond.length) {
+    return false;
+  }
+  for (var i = 0; i < newFirst.length; i++) {
+    if (first[newFirst[i]]) {
+      first[newFirst[i]]++;
+    } else {
+      first[newFirst[i]] = 1;
+    }
+    if (second[newSecond[i]]) {
+      second[newSecond[i]]++;
+    } else {
+      second[newSecond[i]] = 1;
+    }
+    if (first[newFirst[i]] === second[newSecond[i]]) {
+      result = true;
+    } else {
+      result = false;
+      return false;
     }
   }
-  return slicedFirst;
+  return result;
 }
