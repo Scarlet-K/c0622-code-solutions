2bdb7a9b53da34b4155fefc3ffa8b67ad8a1a59a
Scarlet Kim <scarletkim714@gmail.com>
2022-08-24 18:59:30 +0000

    In progress: reverse

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,25 @@
 /* exported reverseWords */
+
+// Create an empty string
+// Go over the characters in the string
+// Stop if the character is a whitespace OR the last char
+// Go over the character in the string backwards until the current char is a whitespace OR the first char
+// Add each current character to the empty string
+// Restart going over the characters from the character after the whitespace
+// Repeat until at last char
+// Return newString
+
+function reverseWords(string) {
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] === ' ') {
+      for (var k = string.length - 1; k >= 0; k--) {
+        newString += string[k];
+        if (string[k] === ' ') {
+          break;
+        }
+      }
+    }
+  }
+  return newString;
+}
