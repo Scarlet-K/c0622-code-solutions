e13d39e1e9449620f8b0e9462a119f0b8ea5d7b6
Scarlet Kim <scarletkim714@gmail.com>
2022-08-25 15:10:17 +0000

    Completed sql-aggregates exercise

--- sql-aggregates/starring-in.sql
+++ sql-aggregates/starring-in.sql
@@ -1,6 +1,8 @@
-select "genres"."name" as "genre",
-       count("castMembers"."actorId")
+select "name" as "genre",
+        count("firstName") as "Lisa Monroe"
   from "castMembers"
   join "filmGenre" using ("filmId")
   join "genres" using ("genreId")
-  group by "genres"."name"
+  join "actors" using ("actorId")
+  where "firstName" = 'Lisa'
+  group by "name";
