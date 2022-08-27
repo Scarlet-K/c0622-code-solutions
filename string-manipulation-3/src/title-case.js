a708074ec3766fa3d7da1ae49d4cfb1484d4cf69
Scarlet Kim <scarletkim714@gmail.com>
2022-08-27 21:32:13 +0000

    In progress: titlecase

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,24 +1,11 @@
 /* exported titleCase */
 
-// var conj = ['and', 'or', 'nor', 'but'];
-// var art = ['a', 'an', 'the'];
-// var prep = ['as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
+// var minor = ['and', 'or', 'nor', 'but', 'a', 'an', 'the', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
 
 function titleCase(title) {
   // debugger;
   // var newTitle = '';
   var lowerCased = title.toLowerCase();
-  var words = [];
-  var word = '';
-  var counter = 0;
-  for (var i = 0; i < lowerCased.length; i++) {
-    word += lowerCased[i]; // adding chars to the empty string
-    if (lowerCased[i] === ' ') { // if the current char is a whitespace
-      for (var k = counter; k <= i; k++) { //
-        words.push(word.trim());
-      }
-    }
-    counter += 1;
-  }
+  var words = lowerCased.split(' ');
   return words;
 }
