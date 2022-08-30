b6a64b63dde6c66336c79efc7ce42ccb491c79e6
Scarlet Kim <scarletkim714@gmail.com>
2022-08-30 20:32:47 +0000

    added comments for future ref

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 function CustomButton(prop) {
-  return <button>{prop.text}</button>;
+  return (
+    <button>{prop.text}</button>
+  );
 }
+
+// function CustomButton() {
+// return <button>Click Me!</button>;
+// }
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 const element = <CustomButton text="Click Me!"/>;
+// const element = <CustomButton/>;
 root.render(element);
+// root.render(<CustomButton />);
