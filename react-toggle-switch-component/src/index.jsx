85d55e49b8b925462da696df8b08937b8015695d
Scarlet Kim <scarletkim714@gmail.com>
2022-08-31 22:40:23 +0000

    Completed react-toggle-switch-component challenge

--- react-toggle-switch-component/src/index.jsx
+++ react-toggle-switch-component/src/index.jsx
@@ -2,15 +2,53 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 class ToggleSwitch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.isClicked === false) {
+      this.setState({
+        isClicked: true
+      });
+    } else {
+      this.setState({
+        isClicked: false
+      });
+    }
+  }
+
   render() {
-    return (
-    <div className='row'>
-      <div className='container-off'>
-        <button className='switch'></button>
-      </div>
-      <span className='col-half text-center align-center'>ON</span>
-    </div>
-    );
+    const clicked = this.state.isClicked;
+    if (clicked) {
+      return (
+        <div className='row align-center'>
+          <div className='col-half'>
+            <div className='container-on'>
+              <button className='switch transform transition' onClick={this.handleClick}></button>
+            </div>
+          </div>
+          <div className='col-half'>
+            <p className='font'>ON</p>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+      <div className='row align-center'>
+          <div className='col-half'>
+            <div className='container-off'>
+            <button className='switch transition' onClick={this.handleClick}></button>
+            </div>
+          </div>
+          <div className='col-half'>
+            <p className='font'>OFF</p>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
