8a21a43d2fb559ceef0f84eb24c9b5281558b5ad
Scarlet Kim <scarletkim714@gmail.com>
2022-09-01 14:36:21 +0000

    Completed react-stopwatch-component challenge

--- react-stopwatch-component/src/index.jsx
+++ react-stopwatch-component/src/index.jsx
@@ -1,56 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-class StopWatch extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      startTick: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState({
-      startTick: !this.state.startTick
-    });
-  }
-
-  // const intervalID = setInterval(setCountdownInterval, 1000);
-  // clearInterval(intervalID);
-
-  // let count = 0;
-
-  // tick() {
-  //   count++;
-  // }
-
-  render() {
-    const tick = this.state.startTick;
-    if (tick) {
-      return (
-        <div className='row text-center'>
-          <div className='col-full'>
-            <button className='watch'>0</button>
-          </div>
-          <div className='col-full'>
-              <i className='fa-sharp fa-solid fa-pause' onClick={this.handleClick}></i>
-          </div>
-        </div>
-      );
-    }
-    return (
-      <div className='row text-center'>
-        <div className='col-full'>
-          <button className='watch'>0</button>
-        </div>
-        <div className='col-full'>
-          <i className='fa-sharp fa-solid fa-play' onClick={this.handleClick}></i>
-        </div>
-      </div>
-    );
-  }
-}
+import StopWatch from './stopwatch';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,1 +1,61 @@
-// import React from 'react';
+import React from 'react';
+
+class StopWatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ticking: false,
+      count: 0
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.startTick = this.startTick.bind(this);
+    this.stopTick = this.stopTick.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  handleClick() {
+    if (this.state.ticking) {
+      this.stopTick();
+    } else {
+      this.startTick();
+    }
+  }
+
+  startTick() {
+    this.intervalID = setInterval(() => {
+      let { count } = this.state;
+      count++;
+      this.setState({ ticking: true, count });
+    }, 1000);
+  }
+
+  stopTick() {
+    clearInterval(this.intervalID);
+    this.setState({ ticking: false, count: this.state.count });
+  }
+
+  reset() {
+    if (!this.state.ticking) {
+      this.setState({ ticking: this.state.ticking, count: 0 });
+    }
+  }
+
+  render() {
+    const tick = this.state.ticking;
+    const count = this.state.count;
+    const pause = 'fa-sharp fa-solid fa-pause';
+    const play = 'fa-sharp fa-solid fa-play';
+    return (
+      <div className='row text-center'>
+        <div className='col-full'>
+          <button className='watch' onClick={this.reset}>{count}</button>
+        </div>
+        <div className='col-full'>
+          <i className={tick ? pause : play} onClick={this.handleClick}></i>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default StopWatch;
